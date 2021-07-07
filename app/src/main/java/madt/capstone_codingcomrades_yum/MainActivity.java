8c27d608564654824d94ae75a827558eafa63d1d
ranjana <ranjana.madt@gmail.com>
2021-07-07 13:04:32 +0530

    Initial Commit

---
+++ app/src/main/java/madt/capstone_codingcomrades_yum/MainActivity.java
@@ -0,0 +1,14 @@
+package madt.capstone_codingcomrades_yum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
