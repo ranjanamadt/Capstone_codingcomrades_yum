c646e0a6ab92ad6edbb463f05008c5abe40a5541
ranjana <ranjana.madt@gmail.com>
2021-07-08 14:46:55 +0530

    functions added in base activity

--- app/src/main/java/madt/capstone_codingcomrades_yum/core/BaseActivity.java
+++ app/src/main/java/madt/capstone_codingcomrades_yum/core/BaseActivity.java
@@ -1,12 +1,31 @@
 package madt.capstone_codingcomrades_yum.core;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BaseActivity  extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
-   protected void yLog(String tag, String message){
-        Log.e(tag,message);
+public class BaseActivity extends AppCompatActivity {
+
+    protected void yLog(String tag, String message) {
+        Log.e(tag, message);
+    }
+
+    protected void yToast(Activity activity, String message) {
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+    }
+    protected static void ySnackbar(Activity activity, String message) {
+        final Snackbar snackbar = Snackbar.make((activity).findViewById(android.R.id.content), message, Snackbar.LENGTH_INDEFINITE);
+        snackbar.setDuration(20000);
+        snackbar.setTextColor(Color.RED);
+        snackbar.setActionTextColor(Color.BLACK);
+        snackbar.setBackgroundTint(Color.LTGRAY);
+        snackbar.setAction("Ok", v -> snackbar.dismiss());
+        snackbar.show();
     }
 }
