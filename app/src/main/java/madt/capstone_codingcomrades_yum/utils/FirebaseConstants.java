1fe420373d718973388ab8130130869d7c7b5258
ranjana <ranjana.madt@gmail.com>
2021-07-14 22:49:00 +0530

    Upgrade DB methods

--- app/src/main/java/madt/capstone_codingcomrades_yum/utils/FirebaseConstants.java
+++ app/src/main/java/madt/capstone_codingcomrades_yum/utils/FirebaseConstants.java
@@ -2,15 +2,15 @@ package madt.capstone_codingcomrades_yum.utils;
 
 public class FirebaseConstants {
    public interface Collections{
-        final static String USERS="users";
+        String USERS="users";
     }
     public interface USER{
-        final static String FIRST_NAME="firstName";
-        final static String LAST_NAME="lastName";
-        final static String DOB="dob";
-        final static String GENDER="gender";
-        final static String SEX_PREFER="sePref";
-        final static String DEVICE_TOKEN="deviceToken";
+        String FIRST_NAME="firstName";
+        String LAST_NAME="lastName";
+        String DOB="dob";
+        String GENDER="gender";
+        String SEX_PREFER="sePref";
+        String DEVICE_TOKEN="deviceToken";
 
     }
 
