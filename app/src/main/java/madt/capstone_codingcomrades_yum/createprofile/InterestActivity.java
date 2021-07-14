9e2958420c05e6ab45650dd0e5ba09aa5c046812
Dinamoljohnson <dinamol4141@gmail.com>
2021-07-14 18:23:49 +0530

    Interests finally done

--- app/src/main/java/madt/capstone_codingcomrades_yum/createprofile/InterestActivity.java
+++ app/src/main/java/madt/capstone_codingcomrades_yum/createprofile/InterestActivity.java
@@ -3,8 +3,13 @@ package madt.capstone_codingcomrades_yum.createprofile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
+
+import com.google.android.material.chip.Chip;
 
 import madt.capstone_codingcomrades_yum.R;
 import madt.capstone_codingcomrades_yum.core.BaseActivity;
@@ -14,26 +19,43 @@ import madt.capstone_codingcomrades_yum.utils.YumTopBar;
 
 public class InterestActivity extends BaseActivity {
     private ActivityInterestsBinding binding;
+    final static String[] topics = {"Art", "Movies", "Sports", "Gym", "Politics"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_interests);
 
-        binding.btnConfirm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(InterestActivity.this,
-                        FoodTopicsActivity.class);
-                startActivity(i);
-            }
-        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setTopBar();
+        binding.interestTopics.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, topics));
+        binding.interestTopics.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                addInterestChip(topics[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        binding.btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.interestTopics.getSelectedItem().toString().isEmpty()) {
+                    Toast.makeText(InterestActivity.this, "'I enjoy eating' field empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent i = new Intent(InterestActivity.this, FoodTopicsActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
 
     @Override
@@ -52,4 +74,9 @@ public class InterestActivity extends BaseActivity {
                 });
     }
 
+    private void addInterestChip(String topic) {
+        Chip newChip = (Chip) getLayoutInflater().inflate(R.layout.yellow_chip, binding.chipInterest, false);
+        newChip.setText(topic);
+        binding.chipInterest.addView(newChip);
+    }
 }
