f3232bb19a69461d929f3627b6a41c842d22ee8e
Sumit <sumitdesai1997@gmail.com>
2021-07-15 15:14:35 -0400

    pre-populating name, dob and gender

--- app/src/main/java/madt/capstone_codingcomrades_yum/createprofile/FinishProfileActivity.java
+++ app/src/main/java/madt/capstone_codingcomrades_yum/createprofile/FinishProfileActivity.java
@@ -1,25 +1,40 @@
 package madt.capstone_codingcomrades_yum.createprofile;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.databinding.DataBindingUtil;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import madt.capstone_codingcomrades_yum.HomeActivity;
 import madt.capstone_codingcomrades_yum.R;
+import madt.capstone_codingcomrades_yum.User;
 import madt.capstone_codingcomrades_yum.core.BaseActivity;
 import madt.capstone_codingcomrades_yum.databinding.ActivityFinishProfileBinding;
+import madt.capstone_codingcomrades_yum.utils.CommonUtils;
 import madt.capstone_codingcomrades_yum.utils.FirebaseCRUD;
+import madt.capstone_codingcomrades_yum.utils.FirebaseConstants;
 import madt.capstone_codingcomrades_yum.utils.YumTopBar;
 
 
@@ -29,6 +44,24 @@ public class FinishProfileActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_finish_profile);
+
+        FirebaseCRUD.getInstance().getDocument(FirebaseConstants.Collections.USERS, FirebaseAuth.getInstance().getUid()).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                yLog("user id :", documentSnapshot.getId() + " ");
+
+                binding.tvUserName.setText(documentSnapshot.getString(FirebaseConstants.USER.FIRST_NAME));
+                binding.tvGender.setText(documentSnapshot.getString(FirebaseConstants.USER.GENDER));
+                binding.tvAge.setText(documentSnapshot.getString(FirebaseConstants.USER.DOB));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                CommonUtils.hideProgress();
+                ySnackbar(FinishProfileActivity.this, getString(R.string.error_saving_not_eat));
+            }
+        });
 
         binding.btnConfirmFinishProfile.setOnClickListener(new View.OnClickListener() {
             @Override
