2f3bb731c2f9305c0771b42a8fd5b2aa3796ef83
Sumit <sumitdesai1997@gmail.com>
2021-07-21 09:52:11 -0400

    adding new activity for profile setting

---
+++ app/src/main/java/madt/capstone_codingcomrades_yum/ProfileSettingActivity.java
@@ -0,0 +1,21 @@
+package madt.capstone_codingcomrades_yum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import madt.capstone_codingcomrades_yum.core.BaseActivity;
+
+public class ProfileSettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setting);
+    }
+
+    @Override
+    protected void setTopBar() {
+
+    }
+}
